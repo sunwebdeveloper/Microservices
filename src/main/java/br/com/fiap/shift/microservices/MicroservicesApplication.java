8c7392192202ce2f9febad628d1636804d29d975
Usuário Convidado <Guest@PA06MICRO15.local>
2018-11-24 12:34:06 -0200

    Configuracao inicial do EurekaServer

---
+++ src/main/java/br/com/fiap/shift/microservices/MicroservicesApplication.java
@@ -0,0 +1,14 @@
+package br.com.fiap.shift.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApplication.class, args);
+	}
+}
